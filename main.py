4db200a1a3614a46526de8f36285b477ba839df8
takashi-uchida <takashi.uchida.office@gmail.com>
2024-05-31 14:33:08 +0900

    update to v1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import uvicorn
 from typing import Union
-from fastapi import FastAPI, Request, Depends, HTTPException, Header
+from fastapi import FastAPI, Request, Depends, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
-from saasus_sdk_python import TenantUserApi
+from saasus_sdk_python.src.auth import TenantUserApi
 from saasus_sdk_python.callback.callback import Callback
 from saasus_sdk_python.middleware.middleware import Authenticate
-from saasus_sdk_python.client.client import SignedApiClient
+from saasus_sdk_python.client.auth_client import SignedAuthApiClient
 
 from dotenv import load_dotenv
 
@@ -15,7 +15,7 @@ app = FastAPI()
 auth = Authenticate()
 callback = Callback()
 # ApiClientを継承したSignedApiClientを使う
-api_client = SignedApiClient()
+auth_api_client = SignedAuthApiClient()
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,7 +31,8 @@ def fastapi_auth(request: Request) -> Union[dict, HTTPException]:
     auth_header = request.headers.get("Authorization", "")
     token = auth_header.replace("Bearer ", "") if "Bearer " in auth_header else ""
     referer = request.headers.get("Referer", "")
-    user_info, error = auth.authenticate(id_token=token, referer=referer)
+    x_saasus_referer = request.headers.get("x_saasus_referer", "")
+    user_info, error = auth.authenticate(id_token=token, referer=referer, x_saasus_referer=x_saasus_referer)
     if error:
         raise HTTPException(status_code=401, detail=str(error))
     return user_info
@@ -63,9 +64,8 @@ def get_tenant_users(auth_user: dict = Depends(fastapi_auth)):
     tenant_id = auth_user.tenants[0].id
 
     try:
-        tenant_user_info = TenantUserApi(api_client=api_client).get_tenant_users(tenant_id=tenant_id,
-                                                                                 _headers=api_client.configuration.default_headers)
-
+        tenant_user_info = TenantUserApi(api_client=auth_api_client).get_tenant_users(tenant_id=tenant_id,
+                                                                                      _headers=auth_api_client.configuration.default_headers)
         return tenant_user_info.users
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
